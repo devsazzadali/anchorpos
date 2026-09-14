f3382c6be0fee0640d78b99eba4446fb77679d28
Sazzad Ali <dev.sazzadali@gmail.com>
2026-09-14 11:07:48 +0600

    fix: disable PWA caching for login routes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
+      urlPattern: /^\/login\/?/i,
+      handler: 'NetworkOnly',
+    },
+    {
+      urlPattern: /^\/home\/?/i,
+      handler: 'NetworkOnly',
+    },
+    {
       urlPattern: /^https:\/\/.*\.supabase\.co\/.*/i,
       handler: 'NetworkFirst',
       options: {
